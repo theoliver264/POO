b796bf3b812865ecbf73d48b0e8be5d220c8937e
Hegar Garcia <hegargarcia@gmail.com>
2018-04-15 13:39:06 -0500

    Adding all methods

--- poo.js
+++ poo.js
@@ -8,3 +8,77 @@ if (!Array.prototype.printValues) {
         for (let index in this) console.log(index);
     }
 }
+
+// Array from
+
+// Array isArray
+
+// Array observe
+
+// Array of
+
+// Array concat
+
+// Array copyWithin
+
+// Array entries
+
+// Array every
+
+// Array fill
+
+// Array filter
+
+// Array find
+
+// Array findIndex
+
+// Array flatMap
+
+// Array flatten
+
+// Array forEach
+
+// Array includes
+
+// Array indexOf
+
+// Array join
+
+// Array keys
+
+// Array lastIndexOf
+
+// Array map
+
+// Array pop
+
+// Array push
+
+// Array reduce
+
+// Array reduceRight
+
+// Array reverse
+
+// Array shift
+
+// Array slice
+
+// Array some
+
+// Array sort
+
+// Array splice
+
+// Array toLocaleString
+
+// Array toSource
+
+// Array toString
+
+// Array unshift
+
+// Array values
+
+// Array [@@iterator]
